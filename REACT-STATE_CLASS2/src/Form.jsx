f2cfb33ad251594e021f873d9e52a6568be12327
ranit <ranitdas739@gmail.com>
2026-03-06 10:44:10 +0530

    label & single set for every element

--- REACT-STATE_CLASS2/src/Form.jsx
+++ REACT-STATE_CLASS2/src/Form.jsx
@@ -9,8 +9,22 @@ export default function Form(){
     }
 
     return (
-        <form>
-            <input placeholder="enter your fullname" type="text"  value={fullName} onChange={hendelchanges}/>
+        <form  onSubmit={handeSubmit}>
+            <label htmlFor="fullName">FullName </label>
+            <input placeholder="enter your fullname" type="text"  value={formData.fullName} onChange={handelformchanges} id="fullName" name="fullName"/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <label htmlFor="userName">UserName </label>
+            <input placeholder="enter your Username" type="text"  value={formData.userName} onChange={handelformchanges}  id="userName" name="userName"/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <label htmlFor="password">Password </label>
+            <input placeholder="enter your PassWord " type="password"  value={formData.password} onChange={handelformchanges}  id="password" name="password"/>
+            <br></br>
+            <br></br>
+            <br></br>
              <button>summit</button>
         </form>
     )
